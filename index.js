a6e649758959061d37cf3e7aa667ac51ebac9d3c
Haoyuan Guo <h62guo@uwaterloo.ca>
2018-01-29 21:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return "Happyholidays, ${name}!";
+}
+/*
+functions happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!";
+}
+
+functions holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!";
+}
+*/
